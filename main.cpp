732a2f504a8a017b96f562dcc01cb3c151f98454
Izwan Azmi <izwanazmi90@gmail.com>
2019-12-10 19:43:56 +0800

    Rename repo

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
-int main( int argc, char *argv[] )
+int main(int argc, char *argv[])
 {
     cv::Mat img = cv::imread( "arnold_schwarzenegger.jpg", cv::IMREAD_COLOR );
 
