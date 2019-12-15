81fcc1c8aef78d130bee5008c5d5f0459c4929bf
Izwan Azmi <izwanazmi90@gmail.com>
2019-12-15 10:03:38 +0800

    Fix cb project file

--- main.cpp
+++ main.cpp
@@ -4,12 +4,12 @@
 
 int main(int argc, char *argv[])
 {
-    cv::Mat img = cv::imread( "arnold_schwarzenegger.jpg", cv::IMREAD_COLOR );
+    cv::Mat img = cv::imread( "photo.jpg", cv::IMREAD_COLOR );
 
     if( img.empty() ) return -1;
 
-    cv::namedWindow( "arnold_schwarzenegger", cv::WINDOW_AUTOSIZE );
-    cv::imshow( "arnold_schwarzenegger", img );
+    cv::namedWindow( "photo", cv::WINDOW_AUTOSIZE );
+    cv::imshow( "photo", img );
     cv::waitKey( 0 );
     return 0;
 }
--- version.h
+++ version.h
@@ -4,7 +4,7 @@
 namespace AutoVersion{
 	
 	//Date Version Types
-	static const char DATE[] = "10";
+	static const char DATE[] = "15";
 	static const char MONTH[] = "12";
 	static const char YEAR[] = "2019";
 	static const char UBUNTU_VERSION_STYLE[] =  "19.12";
@@ -16,17 +16,17 @@ namespace AutoVersion{
 	//Standard Version Type
 	static const long MAJOR  = 1;
 	static const long MINOR  = 0;
-	static const long BUILD  = 0;
-	static const long REVISION  = 0;
+	static const long BUILD  = 1;
+	static const long REVISION  = 3;
 	
 	//Miscellaneous Version Types
 	static const long BUILDS_COUNT  = 1;
-	#define RC_FILEVERSION 1,0,0,0
-	#define RC_FILEVERSION_STRING "1, 0, 0, 0\0"
-	static const char FULLVERSION_STRING [] = "1.0.0.0";
+	#define RC_FILEVERSION 1,0,1,3
+	#define RC_FILEVERSION_STRING "1, 0, 1, 3\0"
+	static const char FULLVERSION_STRING [] = "1.0.1.3";
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY  = 0;
+	static const long BUILD_HISTORY  = 1;
 	
 
 }
