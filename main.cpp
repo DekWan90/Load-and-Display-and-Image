f22349828e44a8da8d94ebc8c320c27c74c8cf46
Izwan Azmi <izwanazmi90@gmail.com>
2019-12-20 12:29:32 +0800

    Update

--- main.cpp
+++ main.cpp
@@ -4,12 +4,17 @@
 
 int main(int argc, char *argv[])
 {
-    cv::Mat img = cv::imread( "arnold_schwarzenegger.jpg", cv::IMREAD_COLOR );
+    cv::Mat img = cv::imread( "monyet.jpg", cv::IMREAD_COLOR );
+    cv::Mat img1 = cv::imread( "monyet1.jpg", cv::IMREAD_COLOR );
 
     if( img.empty() ) return -1;
 
-    cv::namedWindow( "arnold_schwarzenegger", cv::WINDOW_AUTOSIZE );
-    cv::imshow( "arnold_schwarzenegger", img );
+    cv::namedWindow( "monyet", cv::WINDOW_AUTOSIZE );
+    cv::imshow( "monyet", img );
+
+    cv::namedWindow( "monyet1", cv::WINDOW_AUTOSIZE );
+    cv::imshow( "monyet1", img1 );
+
     cv::waitKey( 0 );
     return 0;
 }
